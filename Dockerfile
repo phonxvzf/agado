5ecd470b5685306b650352876f7eb09b1d99743f
Phon Chitnelawong <phon.chitnelawong@gmail.com>
2019-01-27 18:46:35 +0700

    Add Continuous Delivery to GCP (#3)
    * Add continuous delivery
    * Fix permission issue
    * See gcloud output
    * Switch to bash
    * Yes
    * Only deploy on master

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:alpine
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+# Create a non-super group and user
+RUN addgroup -S app\
+  && adduser -S app -G app\
+  && chown -R app /usr/src/app
+
+# Switch to the new user
+USER app
+
+RUN npm install\
+  && npm run build
+
+EXPOSE 3000
+
+# Init database and start server
+CMD [ "npm", "start" ]
---
+++ deploy-ci.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
+source $HOME/google-cloud-sdk/path.bash.inc
+gcloud version
+yes | gcloud components update
+yes | gcloud components update kubectl
+
+docker build -t asia.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
+
+echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
+
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+yes | gcloud --quiet auth configure-docker
+gcloud --quiet config set project $PROJECT_NAME
+gcloud --quiet config set container/cluster $CLUSTER_NAME
+gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+
+docker push asia.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+
+kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=asia.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
