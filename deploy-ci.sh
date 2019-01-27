2a4a6b6afb5a9e8e0c50fd280138f251ff8f7288
Phon Chitnelawong <phon.chitnelawong@gmail.com>
2019-01-27 17:41:54 +0700

    Yes

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -4,16 +4,15 @@ set -e
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
 source $HOME/google-cloud-sdk/path.bash.inc
 gcloud version
-gcloud components update
-gcloud components beta update
-gcloud components update kubectl
+yes | gcloud components update
+yes | gcloud components update kubectl
 
 docker build -t asia.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-gcloud --quiet auth configure-docker
+yes | gcloud --quiet auth configure-docker
 gcloud --quiet config set project $PROJECT_NAME
 gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
