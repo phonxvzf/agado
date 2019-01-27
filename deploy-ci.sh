5e9fd3028d77fe731e32e7ffab6ebaf14b7ea084
Phon Chitnelawong <phon.chitnelawong@gmail.com>
2019-01-27 17:35:54 +0700

    Switch to bash

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
