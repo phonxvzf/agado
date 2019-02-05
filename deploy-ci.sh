91de2e75c09b491a5bd2fd753f2fc974614a923a
Phon Chitnelawong <phon.chitnelawong@gmail.com>
2019-02-05 21:05:01 +0700

    Setup CI for front-end

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -8,6 +8,7 @@ yes | gcloud components update
 yes | gcloud components update kubectl
 
 docker build -t asia.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
+docker build -t asia.gcr.io/${PROJECT_NAME}/${DOCKER_FRONTEND_IMAGE_NAME}:$TRAVIS_COMMIT frontend
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 
@@ -19,5 +20,7 @@ gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
 docker push asia.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+docker push asia.gcr.io/${PROJECT_NAME}/${DOCKER_FRONTEND_IMAGE_NAME}:$TRAVIS_COMMIT
 
 kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=asia.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+kubectl set image deployment/${KUBE_FRONTEND_DEPLOYMENT_NAME} ${KUBE_FRONTEND_DEPLOYMENT_CONTAINER_NAME}=asia.gcr.io/${PROJECT_NAME}/${DOCKER_FRONTEND_IMAGE_NAME}:$TRAVIS_COMMIT
