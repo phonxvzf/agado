b59a91ff492875483b30e406665d93b58c63172f
Phon Chitnelawong <phon.chitnelawong@gmail.com>
2019-02-05 20:17:27 +0700

    Set up front-end workspace

--- migrations/20190204173634_test_table.js
+++ migrations/20190204173634_test_table.js
@@ -2,6 +2,6 @@ exports.up = async (knex) => {
   return knex.raw('CREATE TABLE test (code INTEGER PRIMARY KEY)');
 };
 
-exports.down = function(knex, Promise) {
+exports.down = async (knex) => {
   return knex.raw('DROP TABLE test');
 };
